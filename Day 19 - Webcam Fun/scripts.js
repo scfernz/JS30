a52821030e9ef417eee7149c52fb7570da622b7f
Sean Fernandez <scfernz@gmail.com>
2017-01-26 23:48:31 -0800

    Day 19 - Webcam Fun

--- Day 19 - Webcam Fun/scripts.js
+++ Day 19 - Webcam Fun/scripts.js
@@ -23,7 +23,46 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // Get the pixels, run effects
+    pixels = redEffect(pixels);
+    // pixels = rgbSplit(pixels);
+    // Put it back into the canvas element
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
+function takePhoto() {
+  // play sound
+  snap.currentTime = 0;
+  snap.play();
+  // take data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src=${data} alt="handsome photo"/>`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i] = pixels.data[i] + 100; // red
+    pixels.data[i+1] = pixels.data[i+1] * 0.5; // green
+    pixels.data[i+2] = pixels.data[i+2] * 0.5; // blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 100] = pixels.data[i]; // red
+    pixels.data[i + 200] = pixels.data[i+1]; // green
+    pixels.data[i - 200] = pixels.data[i+2]; // blue
+  }
+  return pixels;
+}
+
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas)
